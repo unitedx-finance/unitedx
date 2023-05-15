3f55d050033eca3859c483023c84b7f369990bc7
0xphen <raph.kifen@gmail.com>
2023-05-15 15:41:30 +0200

    Update name and symbol in Comp.sol contract

--- contracts/Governance/Comp.sol
+++ contracts/Governance/Comp.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.10;
 
 contract Comp {
     /// @notice EIP-20 token name for this token
-    string public constant name = "Compound";
+    string public constant name = "UnitedX";
 
     /// @notice EIP-20 token symbol for this token
-    string public constant symbol = "COMP";
+    string public constant symbol = "UTDX";
 
     /// @notice EIP-20 token decimals for this token
     uint8 public constant decimals = 18;
