229a42d45188f2852d192df6dab5716d9644cb61
0xphen <raph.kifen@gmail.com>
2023-06-06 21:22:39 +0200

    Update Comptroller.sol

--- contracts/Comptroller.sol
+++ contracts/Comptroller.sol
@@ -1466,6 +1466,6 @@ contract Comptroller is ComptrollerV7Storage, ComptrollerInterface, ComptrollerE
      * @return The address of COMP
      */
     function getCompAddress() virtual public view returns (address) {
-        return 0xc00e94Cb662C3520282E6f5717214004A7f26888;
+        return 0x69bd626e5B6044393FdC32938188BeAD6A985b4e;
     }
 }
