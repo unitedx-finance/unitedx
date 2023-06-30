f9a50b43ebd3364afd11e6d433c96468c39f8f3f
0xMiniPanda <0xminipanda@gmail.com>
2023-06-30 10:21:01 +0200

    Oracle should report price in 18 decimals

--- deploy/005-xusdc.js
+++ deploy/005-xusdc.js
@@ -44,7 +44,7 @@ module.exports = async function({ getChainId, getNamedAccounts, deployments }) {
   console.log("Setting price feed source for xUSDC ");
   await priceOracle.setUnderlyingPrice(
     xUsdcDelegator.address,
-    ethers.utils.parseUnits("0.99", 6)
+    ethers.utils.parseUnits("0.99", 18)
   );
 
   const ABI = ["function assetPrices(address asset)"];
